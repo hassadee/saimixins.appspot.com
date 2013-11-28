1433bf3d50efc34f546c5cab35b3e4052b74364d
Hassadee Pimsuwan <hapztron@gmail.com>
2013-11-29 04:28:22 +0700

    Remove reference website files from this repository.

--- assets/js/custom.js
+++
@@ -1,7 +0,0 @@
-/* Disable image draging */
-var imgList = document.getElementsByTagName("img");
-for (var i = 0; i < imgList.length; i++) {
-    imgList[i].ondragstart = function() {
-        return false;
-    }
-}
